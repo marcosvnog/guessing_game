e150d618711697b324fb4dc5fd7ef2444baed55a
marcos <marcosvnogsl@gmail.com>
2023-04-25 23:03:22 -0300

    first commi on guessing_game developing in Rust

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::{thread_rng, Rng};
+
+fn main() {
+    println!("Welcome to the guessing game!!!");
+    
+    let winner_number:u32 = thread_rng().gen_range(1..101);
+    
+    loop {
+        let mut guess = String::new();
+        
+        println!("Try to guess a number");
+
+        io::stdin().read_line(&mut guess).expect("Impossible to read line");
+
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        match guess.cmp(&winner_number) {
+            Ordering::Greater => println!("Too Big..."),
+            Ordering::Less => println!("Too Low..."),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
+}
